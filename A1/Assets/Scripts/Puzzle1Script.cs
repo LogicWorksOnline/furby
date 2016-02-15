33f9eff7e5491197610fd8ac241fc41295220cc1
erolhacisuleyman <erolhacisuleyman@hotmail.com>
2016-02-15 17:31:15 +0200

    first

---
+++ A1/Assets/Scripts/Puzzle1Script.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Puzzle1Script : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter2D(Collision2D collider)
+    {
+
+        gameObject.GetComponent<SpriteRenderer>().color = Color.green;
+
+        collider.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0,2200));
+
+    }
+}
---
+++ A1/Assets/Scripts/TriggerDetection.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerDetection : MonoBehaviour {
+
+    public bool isQuest = false;
+
+    GameObject questImage;
+    GameObject questText;
+
+    public string customText;    
+
+    // Use this for initialization
+    void Start () {
+        questImage = GameObject.Find("QuestImage");
+        questText = GameObject.Find("QuestText");
+
+        //questImage.SetActive(false);
+       // questText.SetActive(false);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    void OnTriggerEnter2D(Collider2D other)
+    {
+       
+        if (other.gameObject.tag == "Tutorial")
+        {
+
+            Destroy(other.gameObject);
+            
+        }
+
+        if (gameObject.tag == "LevelComplete" && other.gameObject.tag == "Player")
+        {
+
+            Application.LoadLevel(1);
+
+        }
+
+        if (gameObject.tag == "Death" && other.gameObject.tag == "Player")
+        {
+
+            Application.LoadLevel(Application.loadedLevel);
+
+        }
+
+        if (gameObject.tag == "GoalD" && other.gameObject.tag == "Player")
+        {
+            Destroy(other.gameObject);
+        }
+
+
+
+        //QUEST
+        if (other.gameObject.tag == "Player" && isQuest)
+        {
+            //questImage.SetActive(true);
+           // questText.active = true;
+
+            
+        }
+
+    }
+}
